2f28f06a5a2f4b71a9665354fefc1bf3b670a0d2
dankozitza <dankoz@gmx.us>
2020-07-27 16:08:46 -0600

    check vector distance

---
+++ src/tools/ogl_utils.cpp
@@ -0,0 +1,63 @@
+//
+// ogl_utils.cpp
+//
+
+#include <GL/glut.h>
+#include <math.h>
+#include "../tools.hpp"
+
+#define EPSILON 0.000001f
+bool tools::line_intersects_triangle(
+    Vertex line[2],
+    Vertex tri[3],
+    Vertex * point
+) {
+    Vertex e0 = tri[1] - tri[0];
+    Vertex e1 = tri[2] - tri[0];
+
+    Vertex dir = line[1] - line[0];
+    Vertex dir_norm = dir.normalize();
+
+    Vertex h = dir_norm.cross(e1);
+    const float a = e0.dot(h);
+
+    if (a > -EPSILON && a < EPSILON) {
+        return false;
+    }
+
+    Vertex s = line[0] - tri[0];
+    const float f = 1.0f / a;
+    const float u = f * s.dot(h);
+
+    if (u < 0.0f || u > 1.0f) {
+        return false;
+    }
+
+    Vertex q = s.cross(e0);
+    const float v = f * dir_norm.dot(q);
+
+    if (v < 0.0f || u + v > 1.0f) {
+        return false;
+    }
+
+    const float t = f * e1.dot(q);
+
+    if (t > EPSILON && t < sqrtf(dir.dot(dir))) { // segment intersection
+        if (point) {
+            *point = line[0] + dir_norm * t;
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+bool tools::verts_within_eps(Vertex v1, Vertex v2, GLfloat eps) {
+   if (abs(v1.x - v2.x) < eps &&
+       abs(v1.y - v2.y) < eps &&
+       abs(v1.z - v2.z) < eps) {
+      return true;
+   }
+   return false;
+}
